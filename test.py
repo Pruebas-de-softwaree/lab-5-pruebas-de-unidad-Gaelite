c875f9b705384936a52d7a51dd0f50581b11ba01
Gael García <129134162+Gaelite@users.noreply.github.com>
2025-10-03 03:03:28 +0000

    TC-005

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ lib.add_book(Book("1984", "George Orwell", 1949, "ISBN001"))
 lib.add_user(User("Anna", "U001"))
 lib.add_user(User("Kira", "U002"))
 
+# Se renta y devuelve un libro
 lib.borrow_book("U002", "ISBN001")
+lib.return_book("U002", "ISBN001")
 
-# lib.borrow_book("U002", "ISBN001")
+# Se renta un libro 
+lib.borrow_book("U002", "ISBN001")
